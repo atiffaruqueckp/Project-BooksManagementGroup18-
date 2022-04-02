f7364be33bb51ba75ecf0240917b54ea06cf3a1b
Atif <atiffaruqueckp@gmail.com>
2022-04-03 00:02:47 +0530

    final sumbit

--- src/middleware/mw.js
+++ src/middleware/mw.js
@@ -4,7 +4,7 @@ const bookModel = require("../models/bookModel")
 const authentication = async function (req, res, next) {
     try {
         let token = req.headers["x-api-key"];
-        if (!token) return res.status(400).send({ status: false, msg: "login is required" })
+        if (!token) return res.status(400).send({ status: false, msg: "token is required" })
         let decodedtoken = jwt.verify(token, "Secret-Key")
         if (!decodedtoken) return res.status(401).send({ status: false, msg: "token is invalid" })
 
